28683dcc5fcf4a03fab6f8b602edef82373b14a6
Nikita <nikita@MacBook-Pro-Nikita.local>
2026-05-06 22:30:43 +0300

    form

--- script.js
+++ script.js
@@ -38,4 +38,92 @@ forwardButton.addEventListener("click", forward);
 backButton.addEventListener("click", back);
 
 
+const form = document.querySelector("#form")
 
+const nameInput = document.querySelector("#name")
+const emailInput = document.querySelector("#email")
+const messageInput = document.querySelector("#message")
+
+const nameError = document.querySelector("#name-error")
+const emailError = document.querySelector("#email-error")
+const messageError = document.querySelector("#message-error")
+
+
+nameInput.addEventListener("input", () => {
+
+    nameError.classList.add("hidden")
+
+    nameInput.classList.remove("error")
+
+})
+
+
+emailInput.addEventListener("input", () => {
+
+    emailError.classList.add("hidden")
+
+    emailInput.classList.remove("error")
+
+})
+
+
+messageInput.addEventListener("input", () => {
+
+    messageError.classList.add("hidden")
+
+    messageInput.classList.remove("error")
+
+})
+
+
+
+form.addEventListener("submit", (e) => {
+
+    e.preventDefault()
+
+    const formData = new FormData(form)
+
+    const name = formData.get("name").trim()
+    const email = formData.get("email").trim()
+    const message = formData.get("message").trim()
+
+    let isValid = true
+
+
+    if(name.length < 4) {
+
+        nameError.classList.remove("hidden")
+
+        nameInput.classList.add("error")
+
+        isValid = false
+    }
+
+
+    if(!email.includes("@")) {
+
+        emailError.classList.remove("hidden")
+
+        emailInput.classList.add("error")
+
+        isValid = false
+    }
+
+
+    if(message.length < 10) {
+
+        messageError.classList.remove("hidden")
+
+        messageInput.classList.add("error")
+
+        isValid = false
+    }
+
+
+    if(isValid) {
+
+        alert("Форма успешно отправлена")
+
+    }
+
+})
